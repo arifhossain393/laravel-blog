1e75ed02e7ba46c1c6dbd18bb2ea20a791cbfb41
Arif Hossain <arifhossain393@gmail.com>
2019-02-16 01:04:44 +0600

    multi authentation system and user role set

---
+++ app/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use App\Role;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','image','about',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function role(){
+        return $this->belongsTo(Role::class);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+<h1>Laravel Blog</h1>
